57d93f5d7ba3feef1311ce4f4901708aebfeb2f1
Johannes Scribante <johannes.scribs@gmail.com>
2023-03-02 21:35:43 +0200

    websocket connection to get the latest ethereum block number

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import { useState } from 'react'
 import './App.css'
+import { Alchemy, Network } from 'alchemy-sdk'
+
+// wss://eth-mainnet.alchemyapi.io/v2/GNauZOAEhjOc34zQQqQuXorOlmC6wJ6Wv
+const settings = {
+    apiKey: 'GNauZOAEhjOc34zQQqQuXorOlmC6wJ6W',
+    network: Network.ETH_MAINNET,
+}
+
+const alchemy = new Alchemy(settings)
 
 function App() {
-  return (
-    <div className="App">
-        blank page
-    </div>
-  )
+    const [block, setBlock] = useState<number|undefined>()
+    // Subscription for new blocks on Eth Mainnet.
+    alchemy.ws.on("block", (blockNumber) => {
+        setBlock(blockNumber)
+    });
+    return (
+        <div className="App">
+            <p>Current block number is { block }</p>
+        </div>
+    )
 }
 
 export default App
