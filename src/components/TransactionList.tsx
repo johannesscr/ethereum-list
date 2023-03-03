78181b21ae67bf6a218eb80ca262418665968efc
Johannes Scribante <johannes.scribs@gmail.com>
2023-03-03 12:43:30 +0200

    added docstrings to transaction list

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -6,14 +6,31 @@ import { CoinGeckoAPI } from '../services/coinGecko.service'
 type TransactionListProps = {
 	transactions: TransactionResponse[];
 }
+
+/**
+ * TransactionList shows a list of all the transactions, it fetches the current
+ * trading price of ethereum in USD to be able to convert ETH to USD value.
+ *
+ * @param transactions the array of block transactions.
+ */
 const TransactionList: FC<TransactionListProps> = ({ transactions }) => {
 	const [ethereumPriceUSD, setEthereumPriceUSD] = useState(0)
+
+	/**
+	 * getUSD is a wrapper function to fetch the current trading price of ETH
+	 * to USD from the Coin Gecko API.
+	 */
 	async function getUSD() {
 		const result = await CoinGeckoAPI.getSimplePrice(['ethereum'], ['usd'])
 		if (result !== undefined) {
 			setEthereumPriceUSD(result.ethereum.usd)
 		}
 	}
+
+	/**
+	 * useEffect to fetch the latest price of ETH to USD when a new set of
+	 * transactions are passed to the component.
+	 */
 	useEffect(() => { getUSD() }, [transactions])
 
 	return (
